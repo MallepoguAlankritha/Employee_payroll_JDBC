ecd396eaa0fdd36c8ff18caff0ecc3e2b083eb6a
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-11-13 15:22:42 +0530

    Added:Establish connection to database

--- main/java/bridelabz/Employeepayroll/EmployeeConfig.java
+++ main/java/bridelabz/Employeepayroll/EmployeeConfig.java
@@ -1,11 +1,24 @@
 package bridelabz.Employeepayroll;
+    import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
 
-public class EmployeeConfig {
+	public class EmployeeConfig {
 
-	public static void main(String[] args) {
-		System.out.println("welcome to employee payroll service");
-		
-
+	    public Connection getConfig(){
+	        String URL_JD = "jdbc:mysql://localhost:3306/Employee_payroll_service_mysql";
+	        String USER_NAME = "root";
+	        String PASSWORD = "Alankritha@12";
+	        Connection connection = null;
+	        try {
+	            Class.forName("com.mysql.jdbc.Driver");
+	            System.out.println("Drivers loaded!!");
+	            connection = DriverManager.getConnection(URL_JD,USER_NAME,PASSWORD);
+	            System.out.println("connection Established!!");
+	        } catch (ClassNotFoundException | SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return connection;
+	    }
 	}
 
-}
---
+++ main/java/bridelabz/Employeepayroll/EmployeePayRoll.java
@@ -0,0 +1,9 @@
+package bridelabz.Employeepayroll;
+
+
+public class EmployeePayRoll {
+    public static void main(String[] args) {
+        EmployeeConfig eConfig = new EmployeeConfig();
+        eConfig.getConfig();
+    }
+}
