5377bbf7549cdf9c4e9ebc2f9eb61963e4dadfe9
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-11-13 14:29:53 +0530

    Added:Welcome to employee payroll

---
+++ main/java/bridelabz/Employeepayroll/EmployeeConfig.java
@@ -0,0 +1,11 @@
+package bridelabz.Employeepayroll;
+
+public class EmployeeConfig {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to employee payroll service");
+		
+
+	}
+
+}
